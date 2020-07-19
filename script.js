fdb0de893d9eef295d6c4c24dc3249f1a88912ca
Kingsley <kingsleyumujeyan@gmail.com>
2020-07-19 22:27:36 +0100

    updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addButton = document.querySelector(".addTodo");
 let input = document.querySelector(".todoInput");
-const itemContainer = document.querySelector(".container");
+const container = document.querySelector(".container");
+const itemContainer = document.querySelector(".item-container");
 const clearBox = document.querySelector(".clear");
 const clearButton = document.querySelector(".clearButton");
 
@@ -21,18 +22,18 @@ class todo{
 
 
         let completeButton = document.createElement("button");
-        completeButton.innerHTML = "<img src='/check.svg' title='Todo Complete'>";
+        completeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check" id="check"><polyline points="20 6 9 17 4 12"></polyline></svg>';
         completeButton.classList.add("completeTodo");
         
         
         
 
         let editButton = document.createElement("button");
-        editButton.innerHTML = "<img src='/edit-3.svg' title='Edit Todo'>";
+        editButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#4111aa" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3" id="edit"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>';
         editButton.classList.add("editTodo");
 
        let deleteButton = document.createElement("button");
-       deleteButton.innerHTML = "<img src='/trash-2.svg' title='Delete Todo'>";
+       deleteButton.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2" id="delete"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>';
        deleteButton.classList.add("deleteTodo");
 
        
@@ -66,7 +67,7 @@ class todo{
     delete(item){
         itemContainer.removeChild(item);
     }
-   
+  
 }
 
 function check(){
